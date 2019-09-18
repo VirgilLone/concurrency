bdd22c625ab9270e0fea6d8be758dc72f85913fb
luo.wen <wyluowen@163.com>
2019-09-18 21:30:33 +0800

    并发容器

--- src/main/java/com/lw/concurrency/concurrencytest/concurrentContainer/ConcurrentHashMapExample.java
+++ src/main/java/com/lw/concurrency/concurrencytest/concurrentContainer/ConcurrentHashMapExample.java
@@ -3,23 +3,17 @@ package com.lw.concurrency.concurrencytest.concurrentContainer;
 import com.lw.concurrency.annotation.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
-/**
- * 几个并发容器
- */
 @Slf4j
 @ThreadSafe
-public class CopyOnWriteArrayListExample {
+public class ConcurrentHashMapExample {
 
     // 请求总数
     public static int clientTotal = 5000;
@@ -27,9 +21,8 @@ public class CopyOnWriteArrayListExample {
     // 同时并发执行的线程数
     public static int threadTotal = 200;
 
-    private static List<Integer> list = new CopyOnWriteArrayList<>();
-//    private static Set<Integer> set = new CopyOnWriteArraySet<>();
-//    private static Set<Integer> set = new ConcurrentSkipListSet<>();
+//    private static Map<Integer, Integer> map = new ConcurrentHashMap<>();
+    private static Map<Integer, Integer> map = new ConcurrentSkipListMap<>();
 
     public static void main(String[] args) throws Exception {
         ExecutorService executorService = Executors.newCachedThreadPool();
@@ -50,12 +43,10 @@ public class CopyOnWriteArrayListExample {
         }
         countDownLatch.await();
         executorService.shutdown();
-        log.info("size:{}", list.size());
+        log.info("size:{}", map.size());
     }
 
     private static void update(int i) {
-        // 几个并发容器CopyOnWriteArrayList、CopyOnWriteArraySet、ConcurrentSkipListSet
-        // 只有对单个元素的操作（add、remove）是原子性的，是线程安全的
-        list.add(i);
+        map.put(i, i);
     }
 }
