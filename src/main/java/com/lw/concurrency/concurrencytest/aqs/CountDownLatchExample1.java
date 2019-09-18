8fa64892e23e6df97ad845ee02499742691a3c1c
luo.wen <wyluowen@163.com>
2019-09-18 22:10:44 +0800

    CountDownLatch

---
+++ src/main/java/com/lw/concurrency/concurrencytest/aqs/CountDownLatchExample1.java
@@ -0,0 +1,45 @@
+package com.lw.concurrency.concurrencytest.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class CountDownLatchExample1 {
+
+    private final static int threadCount = 200;
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            exec.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (Exception e) {
+                    log.error("arise exception...", e);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        //等待CountDownLatch里的计数器减到0才执行后面的语句
+        //countDownLatch.await(10, TimeUnit.MICROSECONDS);
+        countDownLatch.await();
+        log.info("finish...");
+        exec.shutdown();
+    }
+
+    private static void test(int threadNum) throws Exception {
+        Thread.sleep(100);
+        log.info("{}", threadNum);
+        Thread.sleep(100);
+    }
+}
